a68b21c8c7f1e667b7eeb07f3eb27035b673907c
Paloma Cristina Faria <palomacfaria@outlook.com>
2024-03-23 12:31:18 -0300

    Estrutura e personalização

--- portifolio/src/components/Sidebar.jsx
+++ portifolio/src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
 import Avatar from "../img/perfil.jpg";
+import SocialNetworks from "./SocialNetworks";
 import "../styles/components/sidebar.sass";
+import InformationContainer from "./InformationContainer";
+
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Paloma Cristina Faria"/>
       <p className="title">Desenvolvedor</p>
-      <p>Redes sociais</p>
-      <p>Informações de contato</p>
+      <SocialNetworks />
+      <InformationContainer />
       <a href="" className="btn">
         Download currículo
       </a>
