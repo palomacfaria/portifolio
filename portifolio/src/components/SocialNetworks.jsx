2722dec62830865a394be111080dba1ce93c3156
Paloma Cristina Faria <palomacfaria@outlook.com>
2024-04-12 10:32:37 -0300

    Configuração dos links

--- portifolio/src/components/SocialNetworks.jsx
+++ portifolio/src/components/SocialNetworks.jsx
@@ -3,16 +3,16 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
 import "../styles/components/socialnetworks.sass"
 
 const socialNetworks = [
-  { name: "linkedin", icon: <FaLinkedinIn /> },
-  { name: "github", icon: <FaGithub /> },
-  { name: "instagram", icon: <FaInstagram /> },
+  { name: "linkedin", link:"https://www.linkedin.com/in/paloma-cristina-faria-b762631ba/",  icon: <FaLinkedinIn /> },
+  { name: "github", link: "https://github.com/palomacfaria", icon: <FaGithub /> },
+  { name: "instagram", link:"https://www.instagram.com/paloms.faria/", icon: <FaInstagram /> },
 ];
 
 const SocialNetworkContainer = () => {
   return (
     <section id="social-networks">
       {socialNetworks.map((network) => (
-        <a href="#" className="social-btn" id={network.name} key={network.name}>
+        <a href={network.link} target="_blanck" className="social-btn" id={network.name} key={network.name}>
           {network.icon}
         </a>
       ))}
